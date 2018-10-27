e377c5029958b48d3ba721e908831cd1c61aba90
Joanette <joanette.rosario@upr.edu>
2018-10-27 15:21:07 -0400

    system out println

--- src/main/java/com/JoanetteRosario/app/twitter_mapper.java
+++ src/main/java/com/JoanetteRosario/app/twitter_mapper.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -22,27 +23,27 @@ public class twitter_mapper extends Mapper<LongWritable, Text, Text, IntWritable
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
-        String[] tuple = line.split("\\n");
-        String s = "";
-        JSONParser parser = new JSONParser();
-        JSONObject json = null;
-        try {
-            for (int i = 0; i < tuple.length; i++) {
-                String text = (String) json.get("text");
-                String words[]= text.split(" ");
-                int k = words.length;
-                int id = (Integer)json.get("id");
-                for(int j = 0; j<k; j++){
-                    if(words[i].contains("FLU")){
-                        word.set("MAGA");
-                        context.write(new Text("MAGA"), new IntWritable(1));
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println(line);
+        //JSONParser parser = new JSONParser()
+       // JSONArray a = (JSONArray) parser.parse(line);
+//       // try {
+//        //    for (int i = 0; i < tuple.length; i++) {
+//          //      json = (JSONObject)parser.parse(tuple[i]);
+//                String text = (String) json.get("text");
+//                System.out.println(text);
+//                String words[]= text.split(" ");
+//                int k = words.length;
+//                int id = (Integer)json.get("id");
+//                for(int j = 0; j<k; j++){
+//                    if(words[i].contains("MAGA")){
+//                        context.write(new Text("MAGA"), new IntWritable(1));
+//                    }
+//                }
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
