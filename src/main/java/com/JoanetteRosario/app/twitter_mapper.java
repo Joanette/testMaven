0ac26fc46519157332d19888a49ff095c48ea6f2
Joanette <joanette.rosario@upr.edu>
2018-10-28 16:11:31 -0400

    stringbuilder with ids method

--- src/main/java/com/JoanetteRosario/app/twitter_mapper.java
+++ src/main/java/com/JoanetteRosario/app/twitter_mapper.java
@@ -26,30 +26,62 @@ public class twitter_mapper extends Mapper<LongWritable, Text, Text, IntWritable
         JSONParser parser = new JSONParser();
         JSONObject json;
         String text = "";
+
+        StringBuilder magaCollection = new StringBuilder();
+        magaCollection.append("MAGA, ");
+        StringBuilder dicatatorCollection = new StringBuilder();
+        dicatatorCollection.append("Dictator, ");
+        StringBuilder drainCollection = new StringBuilder();
+        drainCollection.append("Drain, ");
+        StringBuilder swampCollection = new StringBuilder();
+        swampCollection.append("Swamp, ");
+        StringBuilder changeCollection = new StringBuilder();
+        changeCollection.append("Change, ");
        // context.write(new Text("boo"), new IntWritable(1));
         for (int i = 0; i < tuple.length; i++) {
             try {
                 json = (JSONObject) parser.parse(tuple[i]);
                 //text = (String) json.get("text");
-                System.out.println(text);
+                //System.out.println(text);
                 String words[] = text.split(" ");
                 int k = words.length;
                 Long id = (Long) json.get("id");
                 for (int j = 0; j < k; j++) {
-                    if (tuple[i].contains("Dictator") && tuple[i].contains("dictator")) {
-                        context.write(new Text("MAGA"), new IntWritable(1));
+                    if (tuple[i].contains("MAGA") && tuple[i].contains("maga")) {
+                       //context.write(new Text("MAGA"), new IntWritable(0));
+                        magaCollection.append(""+id+", " );
                     }
-                    if (tuple[i].contains("Flu") && tuple[i].contains("flu")) {
-                        context.write(new Text("Impeach"), new IntWritable(1));
+                    if (tuple[i].contains("Dictator") && tuple[i].contains("dictator")) {
+                        //context.write(new Text("Impeach"), new IntWritable(0));
+                        dicatatorCollection.append(""+id+", " );
                     }
                     if (tuple[i].contains("Drain") && tuple[i].contains("drain")) {
-                        context.write(new Text("Drain"), new IntWritable(1));
+                        //context.write(new Text("Drain"), new IntWritable(0));
+                        drainCollection.append(""+id+", ");
+                    }
+                    if (tuple[i].contains("Swamp") && tuple[i].contains("swamp")) {
+                       // context.write(new Text("Swamp"), new IntWritable(0));
+                       swampCollection.append(""+id+", ");
+                    }
+                    if (tuple[i].contains("Change") && tuple[i].contains("change")) {
+                       // context.write(new Text("Swamp"), new IntWritable(0));
+                        changeCollection.append(""+id+", ");
                     }
                 }
             } catch (ParseException e) {
                 e.printStackTrace();
             }
         }
+        String magaColle = magaCollection.toString();
+        String dictatorColle = dicatatorCollection.toString();
+        String drainColle = drainCollection.toString();
+        String swampColle = swampCollection.toString();
+        String changeColle = changeCollection.toString();
+        context.write(new Text(magaColle), new IntWritable(0));
+        context.write(new Text(dictatorColle), new IntWritable(0));
+        context.write(new Text(drainColle), new IntWritable(0));
+        context.write(new Text(swampColle), new IntWritable(0));
+        context.write(new Text(changeColle), new IntWritable(0));
     }
 }
 
